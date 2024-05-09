aa8ea9dc383ba5ab7384587c2a67783145403953
markofin0 <m.marullo25@ncf.edu>
2024-05-09 01:24:50 -0400

    portal gun fix part 1
    the portal gun bullets are now deleting previous instantiated portals but now wont teleport the user between portals :(

--- QuestBalloonTemplate-main/UnityQuest2020BalloonTemplate/Assets/BulletScript.cs
+++ QuestBalloonTemplate-main/UnityQuest2020BalloonTemplate/Assets/BulletScript.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class BulletScript : MonoBehaviour
 {
-    public GameObject prefabToDuplicate; 
-    public GameObject originalObject; 
+    public GameObject prefabToDuplicate;
     public bool matchRotationToSurface = true;
-    public GameObject objectToDestroy;
+
+    private GameObject previousPortal;
 
     private void OnCollisionEnter(Collision collision)
     {
-        
         ContactPoint contact = collision.contacts[0];
         Vector3 collisionPoint = contact.point;
         Vector3 collisionNormal = contact.normal;
 
-        
         Quaternion rotation;
         if (matchRotationToSurface)
         {
@@ -24,13 +22,28 @@ public class BulletScript : MonoBehaviour
         }
         else
         {
-            rotation = originalObject.transform.rotation;
+            rotation = transform.rotation;
         }
 
-        
-        Instantiate(prefabToDuplicate, collisionPoint, rotation);
+        // Check if the bullet is blue or red
+        string bulletColor = gameObject.tag; // Assuming bullet color is determined by its tag
 
-        Destroy(objectToDestroy);
-        
+        // Find and destroy the previous portal if it matches the bullet color
+        GameObject[] portals = GameObject.FindGameObjectsWithTag(bulletColor + "portal");
+        foreach (GameObject portal in portals)
+        {
+            if (portal != null && portal != previousPortal)
+            {
+                Destroy(portal);
+            }
+        }
+
+        // Instantiate the new portal
+        GameObject newPortal = Instantiate(prefabToDuplicate, collisionPoint, rotation);
+        previousPortal = newPortal;
+
+        // Destroy the bullet itself
+        Destroy(gameObject);
     }
 }
+
--- QuestBalloonTemplate-main/UnityQuest2020BalloonTemplate/Assets/PortalScript.cs
+++ QuestBalloonTemplate-main/UnityQuest2020BalloonTemplate/Assets/PortalScript.cs
@@ -1,68 +1,56 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PortalScript : MonoBehaviour
 {
-    public Transform RedExit; 
-    public Transform BlueExit; 
-    public GameObject player; 
-    public float teleportCooldown = 1f; 
+    public Transform redExit;
+    public Transform blueExit;
+    public GameObject player;
+    public float teleportCooldown = 1f;
 
     private OVRPlayerController ovrPlayerController;
     private bool isOnCooldown = false;
 
     private void Start()
     {
-        
         ovrPlayerController = player.GetComponent<OVRPlayerController>();
-
     }
 
-    private void OnTriggerEnter(Collider collider)
+    private void OnTriggerEnter(Collider other)
     {
-        
-        if (collider.CompareTag("Player") && !isOnCooldown)
+        if (other.CompareTag("Player") && !isOnCooldown)
         {
-            
             if (CompareTag("RedPortal"))
             {
-                StartCoroutine(TeleportPlayer(BlueExit));
+                StartCoroutine(TeleportPlayer(blueExit));
             }
 
-            
             if (CompareTag("BluePortal"))
             {
-                StartCoroutine(TeleportPlayer(RedExit));
+                StartCoroutine(TeleportPlayer(redExit));
             }
         }
     }
 
     private IEnumerator TeleportPlayer(Transform exitPoint)
     {
-        
         if (ovrPlayerController != null)
         {
             ovrPlayerController.enabled = false;
         }
 
-        
         isOnCooldown = true;
 
-        
         player.transform.position = exitPoint.position;
         player.transform.rotation = exitPoint.rotation;
 
-        
         yield return new WaitForSeconds(teleportCooldown);
 
-        
         if (ovrPlayerController != null)
         {
             ovrPlayerController.enabled = true;
         }
 
-        
         isOnCooldown = false;
     }
 }
