53f67d96d92433b9378a0f4e55abf26b00e783fc
markofin0 <m.marullo25@ncf.edu>
2024-05-16 01:46:23 -0400

    finished level 2 + added level 3 scene

--- UnityQuest2020BalloonTemplate/Assets/Scripts/companioncube.cs
+++ UnityQuest2020BalloonTemplate/Assets/Scripts/companioncube.cs
@@ -3,15 +3,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class companioncube : MonoBehaviour
+public class Companioncube : MonoBehaviour
 {
     public bool activated;
+    public GameObject redCube;
+    public GameObject greenCubePrefab;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "portalbutton")
         {
             Debug.Log("has collide");
             activated = true;
+            Instantiate(greenCubePrefab,redCube.transform.position, Quaternion.identity);
         }
         Debug.Log("WORKS");
     }
